b3326d91c9056680d94d8dfa60303157f75cf31f
hiromiya <hiro.titan.o@gmail.com>
2022-07-05 10:01:25 +0900

    phrase suggesterのサンプルを追加

--- example_01/suggestion.py
+++ example_01/suggestion.py
@@ -8,19 +8,20 @@ import numpy as np
 from elasticsearch import Elasticsearch
 
 
-def mk_suggestion_query(
+def mk_term_suggestion_query(
         text: str,
         size: int
         ) -> Mapping[str, Any]:
-    '''text内の単語で似ているdoc_textフィールドにある単語をサジェストするクエリを返す.
+    '''text内のトークンと似ているdoc_textフィールドにあるトークンをサジェストするクエリを返す.
     
-    追加意味地としては、つづりのミスとかを補正したり、その候補を提示したりする.
+    つづりのミスとかを補正したり、その候補を提示したりするためなどで使用する.
     
     > 参考)
     > 
     > [Suggesters](https://www.elastic.co/guide/en/elasticsearch/reference/current/search-suggesters.html)
 
-    @param text suggest対象となる単語を含むテキスト.
+    @param text suggest対象となるトークンを含むテキスト.
+    @param size トークンごとの最大サジェスト数.
     '''
     return {
       "suggest": {
@@ -37,6 +38,49 @@ def mk_suggestion_query(
       "size": 0
     }
 
+
+def mk_phrase_suggestion_query(
+        text: str,
+        size: int
+        ) -> Mapping[str, Any]:
+    '''textのフレーズに対してフレーズのサジェストを行うするクエリを返す.
+    
+    フレーズの中に綴りのミスなどを補正などに使用するクエリ.\
+    内部的にはトークンのサジェスチョン機能を使っているが、単純に可能性の高いトークンを出力するだけではなく、共起の情報も利用している.
+    
+    > 参考)
+    > 
+    > [Suggesters](https://www.elastic.co/guide/en/elasticsearch/reference/current/search-suggesters.html)
+
+    @param text suggest対象となる単語を含むテキスト.
+    @param size suggestionとして返すフレーズ数（多分）.
+    '''
+    return {
+      "suggest": {
+          "text": text,
+          "phrase_suggestion": {
+            "phrase": {
+              "field": "doc_text.shingle",
+              "size": size,
+              "direct_generator": [
+                {
+                  "field": "doc_text.shingle",
+                  "suggest_mode": "always"
+                },
+                {
+                  "field": "doc_text.reverse",
+                  "suggest_mode": "always",
+                  "pre_filter": "japanese_reverse_analyzer",
+                  "post_filter": "japanese_reverse_analyzer"
+                }
+              ]
+            },
+          }
+      },
+      "size": 0
+    }
+
+
 if __name__ == '__main__':
     index = "resource_test"
 
@@ -45,7 +89,12 @@ if __name__ == '__main__':
     # execute query.
     try:
         # あえてelasticsearchのつづりを間違えてelasticsaerchとしている(aとeを逆転).
-        query = mk_suggestion_query("elasticsaerch lucene fork", 5)
+        query = mk_term_suggestion_query("elasticsaerch lucene fork", 5)
+        res = els_clt.search(index=index, **query)
+        print(json.dumps(res.body, ensure_ascii=False))
+
+        # あえてopensearchとelasticsearchの綴りを間違えている.
+        query = mk_phrase_suggestion_query("opensrch elasticsaerch lucene fork", 5)
         res = els_clt.search(index=index, **query)
         print(json.dumps(res.body, ensure_ascii=False))
     except Exception as e:
